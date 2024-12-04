bf0ead114d83367e3384149d2c41e98cc774d191
goodwebman <gitweb2danya@mail.ru>
2024-12-04 17:59:40 +0600

    created features section with mobile

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Features from '@/components/components/features/Features'
+import Hero from '@/components/components/hero/Hero'
+
+export default function Home() {
+	return (
+		<main>
+			<Hero />
+			<Features />
+		</main>
+	)
+}
