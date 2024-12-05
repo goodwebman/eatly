91ac0ab21cd6cb377abda58070d4716f916e5837
goodwebman <gitweb2danya@mail.ru>
2024-12-05 18:18:04 +0600

    dowload section finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
+import Download from '@/components/components/download/Download'
 import Features from '@/components/components/features/Features'
 import Hero from '@/components/components/hero/Hero'
+
 
 export default function Home() {
 	return (
 		<main>
 			<Hero />
 			<Features />
+			<Download />
 		</main>
 	)
 }
